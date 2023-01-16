4f0a14e056b43e8e92489d5713c5661de8979d09
sedate_0521 <sedate_0521@qq.com>
2023-01-16 16:16:48 +0800

    添加相关注释

--- AdvancedCode/test-MyCollection/src/com/itheima/MyGenericityclass/MyGenericitydemo1.java
+++ AdvancedCode/test-MyCollection/src/com/itheima/MyGenericityclass/MyGenericitydemo1.java
@@ -4,7 +4,10 @@ import javax.swing.*;
 
 public class MyGenericitydemo1 {
     public static void main(String[] args) {
+        // 调用泛型类box 传递类型string，创建对象box1
         box<String> box1 = new box<>();
+
+        // 调用set方法传递变量给泛型类box
         box1.setElement("NI真好看");
 
         box<Integer> box2 = new box<>();
