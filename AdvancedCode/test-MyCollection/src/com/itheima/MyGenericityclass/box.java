ebe7795b07f431c7fa61a5665eca6de2ae1d1e9d
sedate_0521 <sedate_0521@qq.com>
2023-01-16 15:48:45 +0800

    测试idle提交

--- AdvancedCode/test-MyCollection/src/com/itheima/MyGenericityclass/box.java
+++ AdvancedCode/test-MyCollection/src/com/itheima/MyGenericityclass/box.java
@@ -1,6 +1,8 @@
 package com.itheima.MyGenericityclass;
 
 public class box<E> {
+    // 定义一个泛型类
+    // 格式就是在类名后 加入 <X>  这个X可以用任何大写英文字母代替，代指不清楚用户需要用什么类型的变量
     private E element;
 
     public E getElement() {
